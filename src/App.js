67df6ad5ae75a19196743eef3dc6b37c29a44808
imnotonlyone <it.enginer001@gmail.com>
2022-04-20 22:31:50 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
 
+  const tambah = () => {
+    if (counter < 10) {
+      let newCounter = counter + 1;
+      setCounter(newCounter);
+    }
+  };
+
+  const kurang = () => {
+    if (counter > 0) {
+      let kurangCounter = counter - 1;
+      setCounter(kurangCounter);
+    }
+  };
+
   return (
     <div className="App">
       <h1>APP COUNTER REACT</h1>
       <h1>{counter}</h1>
-      <button>Tambah</button>
+      <button onClick={tambah}>Tambah</button>
+      <button onClick={kurang}>Kurang</button>
     </div>
   );
 }
